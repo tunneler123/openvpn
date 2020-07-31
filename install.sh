582dcbda49e5a29331ff505a58bd07b4968eea57
tunneler123 <55013273+tunneler123@users.noreply.github.com>
2020-08-01 00:26:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,11 @@ sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
 wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/tunneler123/openvpn/master/iptables"
-chmod +x /etc/network/if-up.d/iptables
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
 MYIP=`curl -s ifconfig.me`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
+sed -i $MYIP2 /etc/iptables.up.rules;
+iptables-restore < /etc/iptables.up.rules
 service openvpn restart
 wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/tunneler123/openvpn/master/client.conf"
 sed -i $MYIP2 /etc/openvpn/client.ovpn;
