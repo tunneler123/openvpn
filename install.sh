fd7b8c804b64bee3db8b601381160eb308e6ca6b
tunneler123 <55013273+tunneler123@users.noreply.github.com>
2020-10-09 23:07:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,5 +31,7 @@ sudo systemctl start openvpn@server
 service openvpn restart
 cd /usr/bin
 wget -O add-user "https://github.com/tunneler123/openvpn/edit/master/add-user.sh"
+echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
+chmod +x add-user
 clear
 echo DONE INSTALL
